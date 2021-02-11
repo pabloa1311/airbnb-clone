97da96cdefc210c5c58861ad80c4b7a06105d82b
pabloa1311 <pabloa1311@gmail.com>
2021-02-11 17:06:39 -0500

    Search component Created

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import Search from "./Search";
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const handleClick = () => {
+      setShowSearch(!showSearch)
+  }
+
   return (
     <div className="banner">
       <div className="banner__search">
-        <Button className="banner__searchButton" variant="outlined">Search Dates</Button>
+          {showSearch && <Search />}
+        <Button onClick={handleClick} className="banner__searchButton" variant="outlined">
+          Search Dates
+        </Button>
       </div>
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,6 @@ import "./Home.css";
 function Home() {
   return (
     <div className="Home">
-      <h1>Hello Home</h1>
       <Banner />
     </div>
   );
