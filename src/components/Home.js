5bcd2c13ee89c40ecba823b18e054393e856d4a0
pabloa1311 <pabloa1311@gmail.com>
2021-02-11 09:59:31 -0500

    Banner Component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "./Banner";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="Home">
       <h1>Hello Home</h1>
+      <Banner />
     </div>
   );
 }
