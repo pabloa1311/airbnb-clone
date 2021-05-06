5919873dccabbdbbb6b5a0043a50be61dfe15c19
pabloa1311 <pabloa1311@gmail.com>
2021-05-06 17:49:11 -0500

    serachPage component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom" 
+import SearchPage from "./components/SearchPage";
+
+
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
